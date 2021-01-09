94cc11f9cf5d2a0293a328111422adb5ed1edd7e
Samu B <samubalogh@gmail.com>
2021-01-09 21:39:42 +0000

    asdasd

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ from uuid import UUID as pyUUID
 
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 # TODO this is for local dev - to be removed later
