9ef28b36106909dfb638dadc5ce5ea00af1b400d
Samu B <samubalogh@gmail.com>
2021-01-09 19:27:00 +0000

    add queries

--- app.py
+++ app.py
@@ -47,21 +47,27 @@ edges = Table('edges', metadata,
 )
 
 
-single_person_by_name = sql.text("""
-    with
-        person as (
-            select id, properties from nodes where properties->>'name' = :name
-        ), 
-        person_details as (
-            select person.properties, nodes.*, edges.label, edges.head_node from
-                nodes, edges, person where nodes.id = person.id and edges.tail_node = person.id
-        ) 
-        select person_details.*, nodes.properties->>'name' as head_node_name
-            from person_details, nodes 
-            where person_details.head_node = nodes.id
-;""")
+SQL_GET_PERSON_BY_NAME = sql.text("""
+    WITH
+        people AS (
+            SELECT id, properties FROM nodes WHERE properties->>'name' = :name
+        ),
+        person_details AS (
+            SELECT people.id, trim('"' FROM people.properties::text) as properties, edges.label, edges.head_node FROM
+                nodes LEFT OUTER JOIN people ON nodes.id = people.id LEFT OUTER JOIN edges ON edges.tail_node = people.id
+        ),
+        results AS (
+            SELECT
+                person_details.id,
+                person_details.properties AS props,
+                COALESCE(json_object_agg(person_details.label,
+                    json_build_object('name', nodes.properties->>'name', 'id', nodes.id)) FILTER (WHERE person_details.label is not null), '[]') AS outgoing_edges
+                FROM person_details LEFT OUTER JOIN NODES ON person_details.head_node = nodes.id 
+            GROUP BY 1,2)
+        SELECT id, props::json, outgoing_edges FROM results WHERE id IS NOT NULL;
+""")
 
-single_person_by_id = sql.text("""
+SQL_GET_PERSON_BY_ID = sql.text("""
     with
         person as (
             select id, properties from nodes where id = :id
@@ -76,11 +82,52 @@ single_person_by_id = sql.text("""
 ;""")
 
 
+SQL_GET_ALL_PEOPLE = sql.text("""
+    WITH
+        people AS (
+            SELECT id, properties FROM nodes
+        ),
+        person_details AS (
+            SELECT people.id, people.properties, edges.label, edges.head_node FROM
+                nodes LEFT OUTER JOIN people ON nodes.id = people.id LEFT OUTER JOIN edges ON edges.tail_node = people.id
+        ),
+        results AS (
+            SELECT
+                person_details.id,
+                person_details.properties::text AS props,
+                COALESCE(json_object_agg(person_details.label,
+                    json_build_object('name', nodes.properties->>'name', 'id', nodes.id)) FILTER (WHERE person_details.label is not null), '[]') AS outgoing_edges
+                FROM person_details LEFT OUTER JOIN NODES ON person_details.head_node = nodes.id GROUP BY 1,2
+        )
+        SELECT id, props::json, outgoing_edges FROM results WHERE id IS NOT NULL;
+""")
+
+
+SQL_GET_ALL_PEOPLE_FREE_TEXT_SEARCH= sql.text("""
+    WITH
+        people AS (
+            SELECT id, properties FROM nodes join json_each_text(nodes.properties) props ON True WHERE props.value ilike :search_term
+        ),
+        person_details AS (
+            SELECT people.id, people.properties, edges.label, edges.head_node FROM
+                nodes LEFT OUTER JOIN people ON nodes.id = people.id LEFT OUTER JOIN edges ON edges.tail_node = people.id
+        ),
+        results AS (
+            SELECT
+                person_details.id,
+                person_details.properties::text AS props,
+                COALESCE(json_object_agg(person_details.label,
+                    json_build_object('name', nodes.properties->>'name', 'id', nodes.id)) FILTER (WHERE person_details.label is not null), '[]') AS outgoing_edges
+                FROM person_details LEFT OUTER JOIN NODES ON person_details.head_node = nodes.id GROUP BY 1,2
+        )
+        SELECT id, props::json, outgoing_edges FROM results WHERE id IS NOT NULL;
+""")
+
 
 class Person(Resource):
     def get(self, person_id):
         with db.begin() as connection:
-            res = connection.execute(single_person_by_id, id=person_id)
+            res = connection.execute(SQL_GET_PERSON_BY_ID, id=person_id)
 
         results = [dict(i) for i in res]
         for item in results:
@@ -101,11 +148,20 @@ class People(Resource):
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('name')
+        parser.add_argument('search')
+
         arguments = parser.parse_args()
         name = arguments.get('name')
+        search = arguments.get('search')
 
         with db.begin() as connection:
-            res = connection.execute(single_person_by_name, name=name)
+            if name is not None:
+                res = connection.execute(SQL_GET_PERSON_BY_NAME, name=name)
+            elif search is not None:
+                res = connection.execute(SQL_GET_ALL_PEOPLE_FREE_TEXT_SEARCH, search_term=f'%{search}%')
+            else:
+                res = connection.execute(SQL_GET_ALL_PEOPLE)
+
 
         results = [dict(i) for i in res]
         for item in results:
